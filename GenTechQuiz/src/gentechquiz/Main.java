6bcc94ff3a9b0b47d87345c066e3bfe61367fa29
Shashwat Singh <singh.shashwat1709@gmail.com>
2018-07-30 22:09:32 +0530

    Quiz
    It is a quiz game in which there are questions on various fields

---
+++ GenTechQuiz/src/gentechquiz/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gentechquiz;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int a=0;
+    public static int b=0;
+    public static int c=0;
+    public static int d=0;
+    public static int e=0;
+    public static int f=0;
+    public static int g=0;
+    public static int h=0;
+    public static int i=0;
+    public static int j=0;
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
